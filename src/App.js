8d77dcd26ec37c8558aa2d6b93290d618bfeea81
SRC0747 <RjSrc@0747>
2021-10-19 13:41:02 +0530

    Welcome to HelloBridgeLabz React Designing page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import img from './BridgeLabz.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<p>
+BridgeLabz Solutions is one such company based in Mumbai and Bengaluru that offers a free-of-cost intensive four-month fellowship program for aspiring engineers. This company also provides a job placement with some of the leading software companies on completion of this program.
+</p>
+class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
+  constructor() {
+    super()
+    this.state = {
+      title: 'Hello From Bridgelabz !!!',
+      userName: ' ',
+      nameError: 'Something went Wrong'
+    }
+  }
+  onClick = ($event) => {
+    console.log("Save Button is Clicked !", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onNameChange = ($event) => {
+    console.log("value is ", $event.target.value);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2}$');
+    this.setState({userName: $event.target.value})
+    if (nameRegex.test($event.target.value)) {
+      this.setState({nameError: ''})
+    } else {
+      this.setState({nameError: 'Name is Incorrect'})
+    }
+    }
+
+  render() {
+    return (
+      <div>
+        <div>
+        <h1>{this.state.title}</h1>
+        <h2>Hello {this.state.userName} from bridgelabz</h2>
+        <img src={img} onClick={this.onClick}
+        alt="This Bridgelabz logo: we gaurantee your dream job !" />
+        </div>
+        <div>
+        <input onChange={this.onNameChange} />
+        <span className="error-output">{this.state.nameError}</span>
+        </div>
+      </div>
+      
+    );
+  }
 }
-
 export default App;
